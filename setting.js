7617485ca505ef5a89bd0ad89963bdb9e3a25a28
dtc225010019-sys <dtc225010019@ictu.edu.vn>
2025-09-08 09:51:32 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,10 @@
+module.exports = {
+    uiPort: process.env.PORT || 1880,
+    flowFile: 'flows.json',
+    credentialSecret: false,
+    editorTheme: {
+        projects: {
+            enabled: false
+        }
+    }
+}
